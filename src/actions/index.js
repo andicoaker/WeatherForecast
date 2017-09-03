2fbbc6b416d532668e90f5d3e7507037559eacc2
Andrea Coaker <andicoaker@gmail.com>
2017-09-03 19:12:48 -0400

    created search-bar and submit event, added weather API key and set-up ajax request using axios, created app component

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_KEY = '4fd51d9b10d2d6e856393d25204437ae';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+// fetchWeather action creator
+// takes city as part of the search query
+// using axios library to get ajax request and return a promise (request). promise is then passed into actions payload.
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+
+}
